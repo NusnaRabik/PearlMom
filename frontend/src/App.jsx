d9e51e8f00b55be38ffb912df3a859983b42160f
kanishka-liyanage <22cse0375@ms.sab.ac.lk>
2026-05-09 12:59:25 +0530

    create landing page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LandingPage from './pages/public/LandingPage';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-50 flex flex-col">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
